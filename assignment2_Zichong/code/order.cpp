84e7f2b64723ef12e73fa82052b922ed42656d5b
jackgloves <81619132+jackgloves@users.noreply.github.com>
2023-07-07 21:23:47 +0800

    Delete assignment2_Zichong directory

--- assignment2_Zichong/code/order.cpp
+++
@@ -1,9 +0,0 @@
-#include "order.h"
-
-std::ostream& operator<<(std::ostream& os, const Order& order) {
-    os << "price=" << order.price
-        << " open_qty=" << order.open_qty
-        << " order_id=" << order.order_id
-        << " is_sell=" << static_cast<int>(order.is_sell);
-    return os;
-}
